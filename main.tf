4dca8bbc230e73a6c8deb79853b59d7cc85b65a4
WaraKx <147081102+WaraKx@users.noreply.github.com>
2024-03-27 17:28:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Configuration du provider Azure
 terraform {
   required_providers {
     azurerm = {
@@ -9,4 +10,36 @@ terraform {
 }
 provider "azurerm" {
   features {}
+}
+
+# Déclaration du groupe de ressources Azure
+resource "azurerm_resource_group" "example" {
+  name     = "rg-votre_nom-{remplacez_par_votre_nom}-${random_integer.example.result}"
+  location = "West Europe"
+}
+
+# Déclaration du plan App Service
+resource "azurerm_app_service_plan" "example" {
+  name                = "asp-votre_nom-{remplacez_par_votre_nom}-${random_integer.example.result}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+# Déclaration de l'application web
+resource "azurerm_app_service" "example" {
+  name                = "webapp-votre_nom-{remplacez_par_votre_nom}-${random_integer.example.result}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    java_version     = "1.7"
+    java_container   = "JAVA"
+    java_container_version = "17"
+  }
 }
