e5ee51d756aab09632b3145ebb2b5e2468a795a6
WaraKx <147081102+WaraKx@users.noreply.github.com>
2024-03-27 17:43:32 +0100

    Create main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# Configuration du provider Azure
 terraform {
   required_providers {
     azurerm = {
@@ -10,36 +9,4 @@ terraform {
 }
 provider "azurerm" {
   features {}
-}
-
-# Déclaration du groupe de ressources Azure
-resource "azurerm_resource_group" "example" {
-  name     = "rg-Kyllian-alicia-{Kyllian-alicia}-${random_integer.example.result}"
-  location = "West Europe"
-}
-
-# Déclaration du plan App Service
-resource "azurerm_app_service_plan" "example" {
-  name                = "asp-Kyllian-alicia-{Kyllian-alicia}-${random_integer.example.result}"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  kind                = "Linux"
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-}
-
-# Déclaration de l'application web
-resource "azurerm_app_service" "example" {
-  name                = "webapp-Kyllian-alicia-{Kyllian-alicia}-${random_integer.example.result}"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-
-  site_config {
-    java_version     = "1.7"
-    java_container   = "JAVA"
-    java_container_version = "17"
-  }
 }
